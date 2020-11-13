6d904ac45693faaff319d8c8a5b0871cafc8db99
NicKomarov73 <komarov@lic145.kiev.ua>
2020-11-13 11:55:35 +0200

    add some comments

--- calc.py
+++ calc.py
@@ -1,35 +1,17 @@
 from tkinter import *
 
-global open_fr
-open_fr = TRUE
-
 # creating window
 window = Tk()
 window.title("Test")
 window.geometry('300x390')
 window.resizable(False, False)
 
-# some funcions
-def com1():
-    fr.pack_forget()
+# creating  menu frame
+menu_fr = Frame(window, bg='#3E4048')
+menu_fr.pack(fill=BOTH, expand=1)
 
-def com2():
-    fr.pack(side=TOP)
-
-# creating buttons
-btn = Button(window, text='Close', command=com1)
-btn.pack(side=TOP)
-btn2 = Button(window, text='OpenUp', command=com2)
-btn2.pack(side=RIGHT)
-
-# creating frame
-fr = Frame(window, bg='red', bd=5)
-btn_fr1 = Button(fr, text='l', width=5, height=2)
-btn_fr2 = Button(fr, text='o', width=5, height=2)
-btn_fr3 = Button(fr, text='l', width=5, height=2)
-btn_fr1.pack()
-btn_fr2.pack()
-btn_fr3.pack()
-fr.pack(side=TOP)
+# add buttons to menu frame
+start_btn = Button(menu_fr, bg = 'black', text = 'Start', fg = 'white')
+start_btn.pack(expand = 1)
 
 window.mainloop()
