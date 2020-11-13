0c8e437d2bc7333765b5f4488013e612955c3d88
NicKomarov73 <komarov@lic145.kiev.ua>
2020-11-13 11:06:24 +0200

    add some comments

--- calc.py
+++ calc.py
@@ -9,19 +9,20 @@ window.title("Test")
 window.geometry('300x390')
 window.resizable(False, False)
 
-
+# some funcions
 def com1():
     fr.pack_forget()
 
 def com2():
     fr.pack(side=TOP)
 
-
+# creating buttons
 btn = Button(window, text='Close', command=com1)
 btn.pack(side=TOP)
 btn2 = Button(window, text='OpenUp', command=com2)
 btn2.pack(side=RIGHT)
 
+# creating frame
 fr = Frame(window, bg='red', bd=5)
 btn_fr1 = Button(fr, text='l', width=5, height=2)
 btn_fr2 = Button(fr, text='o', width=5, height=2)
